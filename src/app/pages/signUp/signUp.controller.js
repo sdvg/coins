04ceef23f58ec09e5d5f0f2c202d3b349c7119fd
Stefan Dietz <dev@stefan-dietz.eu>
2016-07-13 23:11:13 +0200

    Implemented workaround for 401s after sign up

--- src/app/pages/signUp/signUp.controller.js
+++ src/app/pages/signUp/signUp.controller.js
@@ -12,7 +12,10 @@ function SignUpController($state, account) {
     account.signUp(credentials).then(
         () => {
           hoodie.account.signIn(credentials).then(
-              () => $state.go('overview'),
+              () => {
+                //workaround for https://github.com/hoodiehq/hoodie/issues/503
+                location.href = $state.href('overview')
+              },
               (error) => {
                 this.errorMessage = `You have been signed up successful but the following error occurred at auto-login:
                                      ${error.message}`;
