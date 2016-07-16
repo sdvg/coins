3de3998a24ba2eef023390976319f098f7e2f6fb
Stefan Dietz <dev@stefan-dietz.eu>
2016-07-16 21:54:14 +0200

    Added icons & web manifest

--- config/webpack/global.js
+++ config/webpack/global.js
@@ -1,19 +1,19 @@
 'use strict';
 
 // Depends
-var path              = require('path');
-var webpack           = require('webpack');
-var autoprefixer      = require('autoprefixer-core');
-var Manifest          = require('manifest-revision-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer-core');
+var Manifest = require('manifest-revision-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var NODE_ENV = process.env.NODE_ENV || "production";
-var DEVELOPMENT  = NODE_ENV === "production" ? false : true;
+var DEVELOPMENT = NODE_ENV === "production" ? false : true;
 var stylesLoader = 'css-loader?sourceMap!postcss-loader!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true';
 
 
-module.exports = function(_path) {
+module.exports = function (_path) {
   var rootAssetPath = _path + 'src';
 
   var webpackConfig = {
@@ -27,7 +27,7 @@ module.exports = function(_path) {
     // output system
     output: {
       path: 'dist',
-      filename: '[name].js',
+      filename: '[name]_[hash].js',
       publicPath: '/'
     },
 
@@ -36,10 +36,10 @@ module.exports = function(_path) {
       extensions: ['', '.js'],
       modulesDirectories: ['node_modules'],
       alias: {
-        _appRoot:     path.join(_path, 'src', 'app'),
-        _images:      path.join(_path, 'src', 'app', 'assets', 'images'),
+        _appRoot: path.join(_path, 'src', 'app'),
+        _images: path.join(_path, 'src', 'app', 'assets', 'images'),
         _stylesheets: path.join(_path, 'src', 'app', 'assets', 'styles'),
-        _scripts:     path.join(_path, 'src', 'app', 'assets', 'js')
+        _scripts: path.join(_path, 'src', 'app', 'assets', 'js')
       }
     },
 
@@ -49,7 +49,7 @@ module.exports = function(_path) {
       loaders: [{
         test: /\.html$/,
         loaders: [
-          'ngtemplate-loader?relativeTo='+ _path,
+          'ngtemplate-loader?relativeTo=' + _path,
           'html-loader?attrs[]=img:src&attrs[]=img:data-src'
         ]
       }, {
@@ -94,20 +94,22 @@ module.exports = function(_path) {
       }, {
         test: /\.(jpe?g|png|gif)$/i,
         loaders: [
-          'url-loader?name=assets/images/[name]_[hash].[ext]&limit=10000'
+          'url-loader?name=assets/images/[name]_[hash].[ext]&limit=1'
         ]
       }, {
         test: require.resolve("angular"),
         loaders: [
           "expose?angular"
         ]
+      }, {
+        test: /webmanifest.json$/,
+        loader: 'file-loader?name=[name]_[hash].[ext]!web-app-manifest-loader'
       },
-
       ]
     },
 
     // post css
-    postcss: [autoprefixer({ browsers: ['last 5 versions'] })],
+    postcss: [autoprefixer({browsers: ['last 5 versions']})],
 
     // load plugins
     plugins: [
@@ -135,10 +137,10 @@ module.exports = function(_path) {
         rootAssetPath: rootAssetPath,
         ignorePaths: ['.DS_Store']
       }),
-      new ExtractTextPlugin('assets/styles/css/[name]' + (NODE_ENV === 'development' ? '' : '.[chunkhash]') + '.css', { allChunks: true }),
+      new ExtractTextPlugin('assets/styles/css/[name]' + (NODE_ENV === 'development' ? '' : '.[chunkhash]') + '.css', {allChunks: true}),
       new HtmlWebpackPlugin({
         filename: 'index.html',
-        template: path.join(_path, 'src', 'tpl-index.html')
+        template: path.join(_path, 'src', 'tpl-index.ejs')
       })
     ],
   };
